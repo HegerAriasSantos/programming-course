eeed074a35fc0d4927a2839e2ef8bbf29e375653
Heger Arias Santos <hegel.0218@gmail.com>
2023-02-09 23:49:10 -0400

    9/2/2023

--- typescript/8_2_2023.ts
+++ typescript/8_2_2023.ts
@@ -9,29 +9,30 @@
 // };
 // //  datos primitivos
 
-// let numerico: number = 10;
-// let texto: string = "hola mundo";
-// let boleano: boolean = true;
-// let indefinido: undefined = undefined;
-// let vacio: null = null;
+let numerico: number = 10;
+let texto: string = "hola mundo";
+let boleano: boolean = true;
+let indefinido: undefined = undefined;
+let vacio: null = null;
+let NaN2 = Number("hola"); // NaN es un tipo de dato que se crea cuando se hace una mala conversion de string a numero
 
 // // datos complejos
 
-// let arreglo: number[] = [4, 6, 7, 2, 3, 1, 5, 6, 8, 8, 10, 12];
-// let arreglo2: Array<number | string> = [
-// 	4,
-// 	6,
-// 	"hola mundo",
-// 	2,
-// 	3,
-// 	1,
-// 	5,
-// 	6,
-// 	8,
-// 	8,
-// 	10,
-// 	12,
-// ];
+let arreglo: number[] = [4, 6, 7, 2, 3, 1, 5, 6, 8, 8, 10, 12];
+let arreglo2: Array<number | string> = [
+	4,
+	6,
+	"hola mundo",
+	2,
+	3,
+	1,
+	5,
+	6,
+	8,
+	8,
+	10,
+	12,
+];
 
 // let persona: Persona = {
 // 	name: "",
@@ -60,95 +61,97 @@
 // // para igualdad  ==
 // // console.log(5 === 10)
 
+// ! =  !=  no es igual
+
 // // mayor que  >=
 // // menor que  <=
 
 // // Logicos
-// let isAraay: boolean = false;
-// let isAlgo: boolean = false;
+let isAraay: boolean = false;
+let isAlgo: boolean = false;
 
 // // ambos && And
 // // uno u otro || OR
 
-// console.log(isAlgo && isAraay);
+console.log(isAlgo && isAraay);
 
 // Estructura de control
 
-// let a = 10; variable
-// const b = 20; constante
+let a = 10; // variable
+var c = 20; // variable (no usar)
+const b = 20; // constante
 
-// a = 20;
-// b= 30
+a = 20;
+// b = 30;
 
 ////// condicionales
 
-// if (isAlgo) {
-// 	if (isAraay) {
-// 		console.log("es array");
-// 	} else {
-// 		console.log("no es array");
-// 	}
-// 	console.log("algo");
-// } else {
-// 	console.log("no algo");
-// }
+if (isAlgo) {
+	if (isAraay) {
+		console.log("es array");
+	} else {
+		console.log("no es array");
+	}
+	console.log("algo");
+} else {
+	console.log("no algo");
+}
 
-// if (isAlgo) console.log("es algo");
-// else console.log("no es algo");
+if (isAlgo) console.log("es algo");
+else console.log("no es algo");
 
-// if (isAlgo) {
-// 	console.log("algo");
-// }
-// else if (isArray) {
-// console.log('es array')
-// }
+if (isAlgo) {
+	console.log("algo");
+} else if (isAraay) {
+	console.log("es array");
+}
 
-// if (isAlgo || isAraay) {
-// 	console.log("no algo2");
-// } else {
-// 	console.log("no algo3");
-// }
+if (isAlgo || isAraay) {
+	console.log("no algo2");
+} else {
+	console.log("no algo3");
+}
 
-// let valorSeleccionado = 2;
-// let diasDeLaSemana = [
-// 	"lunes",
-// 	"martes",
-// 	"miercoles",
-// 	"jueves",
-// 	"viernes",
-// 	"sabado",
-// 	"domingo",
-// ];
+let valorSeleccionado = 2;
+let diasDeLaSemana = [
+	"lunes",
+	"martes",
+	"miercoles",
+	"jueves",
+	"viernes",
+	"sabado",
+	"domingo",
+];
 
-// diasDeLaSemana[valorSeleccionado];
+console.log(diasDeLaSemana[valorSeleccionado]); // miercoles
 
-// switch (valorSeleccionado) {
-// 	case 0:
-// 		console.log("lunes");
-// 		break;
-// 	case 1:
-// 		console.log("martes");
-// 		break;
-// 	case 2:
-// 		console.log("miercoles");
-// 		break;
-// 	case 3:
-// 		console.log("jueves");
-// 		break;
-// 	case 4:
-// 		console.log("viernes");
-// 		break;
-// 	case 5:
-// 		console.log("sabado");
-// 		break;
-// 	case 6:
-// 		console.log("domingo");
-// 		break;
+switch (valorSeleccionado) {
+	case 0:
+		console.log("lunes");
+		break;
+	case 1:
+		console.log("martes");
+		break;
+	case 2:
+		console.log("miercoles");
+		break;
+	case 3:
+		console.log("jueves");
+		break;
+	case 4:
+		console.log("viernes");
+		break;
+	case 5:
+		console.log("sabado");
+		break;
+	case 6:
+		console.log("domingo");
+		break;
 
-// 	default:
-// 		console.log("no es un dia de la semana");
-// 		break;
-// }
+	default:
+		console.log("no es un dia de la semana");
+		break;
+}
 
 ///// Ciclicas
 
@@ -168,6 +171,12 @@ let condicion = 1;
 while (condicion !== 0) {
 	condicion = Number(prompt("eligir condicion"));
 }
+
+// (No usar) primero ejecuta el codigo al menos una vez y luego evalua para saber si volvera a correr ese codigo
+
+do {
+	condicion = Number(prompt("eligir condicion"));
+} while (condicion !== 0);
 
 // la funcion consta de 3 partes: inputs (datos que se le da o parametros), proceso (lo que sucede dentro de la funcion), salida: valor que se retorna de la funcion
 
---
+++ typescript/9_2_2023.ts
@@ -0,0 +1,53 @@
+// Imprimir por consola la tabla del 1-12 con sus primeros 5 multiplos
+
+// for, while, dowhile, if, function
+
+// for (let i = 1; i <= 12; i++) {
+// 	// 1, 2
+// 	for (let j = 1; j <= 5; j++) {
+// 		// j 1 - 5
+// 		// console.log(i, " * ", j, " = ", i * j);
+// 		console.log(`${i} * ${j} = ${i * j}`);
+// 		// 1 * 1 = 1
+// 	}
+// }
+
+// // con while
+// let i = 1;
+// let j = 1;
+// while (i <= 12) {
+// 	while (j <= 5) {
+// 		// j 1 - 5
+// 		// console.log(i, " * ", j, " = ", i * j);
+// 		console.log(`${i} * ${j} = ${i * j}`);
+// 		// 1 * 1 = 1
+// 		j++;
+// 	}
+// 	j = 1;
+
+// 	i++;
+// }
+
+/* Imprimir por consola las tablas del x (valor dado por el usuario) por los multiplos j (valor dado por el usuario) y que se vea tabla completa de x completa por iteracion en un solo string ej:
+
+primera iteracion
+  " 1 x 1 = 1
+    1 x 2 = 2
+    1 x 3 = 3
+    1 x 4 = 4
+    1 x 5 = 5
+    1 x 6 = 6
+  "
+
+segundo iteracion
+  " 2 x 1 = 2
+    2 x 2 = 4
+    2 x 3 = 6
+    2 x 4 = 8
+    2 x 5 = 10
+    2 x 6 = 12
+  "
+
+PD: iteracion es cada ejecucion de una estructura de control ciclica
+
+*/
